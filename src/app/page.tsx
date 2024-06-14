e77c787801216b8b9f0e10b8ad1f9d53f3049336
David Leon <leon95024@gmail.com>
2024-06-14 03:11:10 -0500

    refactor: Update page metadata
    Remove unnecessary line break in the page metadata for improved code readability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title:
     "Servicios Profesionales de Topografía - Gestión Catastral, Diseño de Vías, Fotogrametría y LiDAR",
-
   description:
     "Descubre nuestro portafolio de servicios de topografía especializados: gestión catastral, diseño de vías, fotogrametría y LiDAR, georreferenciación y parcelaciones. Garantizamos precisión y calidad en cada proyecto para satisfacer tus necesidades de infraestructura y mapeo.",
   keywords:
